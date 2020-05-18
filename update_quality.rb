d61c143ef5a9714b1ee1360d3fedb84bf94e16f2
Daniel Thompson <DanThomp507@gmail.com>
2020-05-18 14:23:09 -0400

    started to refactor - updated update_quality method

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,19 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
+    case award.name
+    when 'Blue Distinction Plus'
+      handle_blue_distinction_plus(award)
+    when 'Blue Star'
+      handle_blue_star(award)
+    when 'Blue First'
+      handle_blue_first(award)
+    when 'Blue Compare'
+      handle_blue_compare(award)
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
+      handle_normal(award)
     end
   end
 end
+
+# TODO: write handle methods for complete refactoring
